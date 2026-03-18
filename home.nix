b791b2832ed9fdfb9007f571a068ce887b5ccfd5
Seth Henderson <ivanhen99@gmail.com>
2026-03-18 18:12:55 +1100

    Added additional hyprland binds

--- home.nix
+++ home.nix
@@ -14,6 +14,7 @@
     obsidian
     jellyfin-desktop
     nerd-fonts.jetbrains-mono
+    brightnessctl
   ];
   programs.bash.enable = true;
 
@@ -72,6 +73,35 @@
         "$mod, R, exec, $menu"
         "$mod, P, pseudo,"
         "$mod, J, layoutmsg, togglesplit"
+
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+      ]
+      ++ (
+        builtins.concatLists (builtins.genList (i:
+          let ws = i + 1;
+          in [
+            "$mod, code:1${toString i}, workspace, ${toString ws}"
+            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+          ]
+        )
+        9)
+      );
+
+      bindm = [
+        "$mod, mouse:272, movewindow"
+        "$mod, mouse:273, resizewindow"
+      ];
+
+      bindel = [
+        ",XF86AudioRaiseVolume, exec, wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"
+        ",XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+        ",XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+        ",XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+        ",XF86MonBrightnessUp, exec, brightnessctl -e4 -n2 set 5%+"
+        ",XF86MonBrightnessDown, exec, brightnessctl -e4 -n2 set 5%-"
       ];
     };
   };

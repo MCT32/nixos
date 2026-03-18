56e4557a40b7d44e0118246cae496600bc025968
Seth Henderson <ivanhen99@gmail.com>
2026-03-18 17:46:37 +1100

    Added home wifi + priorities

--- configuration.nix
+++ configuration.nix
@@ -25,10 +25,12 @@ in
     secretsFile = "/etc/nixos/wireless.conf";
 
     networks.DsquariusGreenJr = {
+      priority = 1;
       psk = "11111111";
     };
 
     networks.eduroam = {
+      priority = 2;
       auth = ''
         key_mgmt=WPA-EAP
         eap=PEAP
@@ -36,6 +38,11 @@ in
         identity="105751853@swin.edu.au"
         password="o4uKc5XM9!LoetbJ6y3Ruznl66oxq"
       '';
+    };
+
+    networks."TP-Link_41BC" = {
+      priority = 2;
+      psk = "94969740";
     };
   };
 

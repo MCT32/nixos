a6368494a769d9bdb1fbc9f44bd9433af768f249
Seth Henderson <ivanhen99@gmail.com>
2026-05-18 13:52:52 +1000

    Added audio related packages

--- hosts/common/optional/pipewire.nix
+++ hosts/common/optional/pipewire.nix
@@ -1,4 +1,7 @@
 {
+  pkgs,
+  ...
+}: {
   security.rtkit.enable = true;
   services.pulseaudio.enable = false;
   services.pipewire = {
@@ -8,4 +11,10 @@
     pulse.enable = true;
     jack.enable = true;
   };
+
+  # Extra audio packages
+  environment.systemPackages = with pkgs; [
+    pavucontrol
+    crosspipe
+  ];
 }

0e0705a120b62509583bf2334e2a43abc959e3cc
Seth Henderson <ivanhen99@gmail.com>
2026-05-25 14:52:42 +1000

    qutebrowser: Updated user agent

--- home/sethh/features/desktop/common/qutebrowser.nix
+++ home/sethh/features/desktop/common/qutebrowser.nix
@@ -26,9 +26,7 @@
     };
 
     settings = {
-      # TODO: Change to chrome linux for best compatibility
-      # Required to log in to google
-      content.headers.user_agent = "Mozilla/5.0 ({os_info}; rv:135.0) Gecko/20100101 Firefox/135";
+      content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36";
 
       url.start_pages = [ "http://192.168.0.238/" ];
     };

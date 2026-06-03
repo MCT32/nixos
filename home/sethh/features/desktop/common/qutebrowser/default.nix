31f8e3d99ea508f6c3f2dd89962d349e88bb4f6d
Seth Henderson <ivanhen99@gmail.com>
2026-06-04 07:30:49 +1000

    qutebrowser: Changed homepage to use tailscale

--- home/sethh/features/desktop/common/qutebrowser/default.nix
+++ home/sethh/features/desktop/common/qutebrowser/default.nix
@@ -22,13 +22,13 @@
       yt = "https://youtube.com";
 
       # Home server
-      home = "http://192.168.0.238/";
+      home = "http://hal9000/";
     };
 
     settings = {
       content.headers.user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Safari/537.36";
 
-      url.start_pages = [ "http://192.168.0.238/" ];
+      url.start_pages = [ "http://hal9000/" ];
     };
   };
 }

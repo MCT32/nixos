45147ad5ec53012727dbc15fa29c9bacfe7c8951
Seth Henderson <ivanhen99@gmail.com>
2026-06-11 15:23:34 +1000

    fcitx: Added fcitx5 with ilo-sitelen

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,5 +1,36 @@
-{ lib, ... }: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   i18n = {
+    inputMethod = {
+      enable = true;
+      type = "fcitx5";
+      fcitx5.addons = with pkgs; [
+        (pkgs.stdenv.mkDerivation {
+          pname = "ilo-sitelen";
+          version = "unstable-2023";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "0x182d4454fb211940";
+            repo = "ilo-sitelen";
+            rev = "master";
+            sha256 = "sha256-caQVPBPuZjOwbtcDhxAdmG7PHXe50OeSLkSBoCtMcrQ="; # Replace with actual hash if mismatched
+          };
+
+          nativeBuildInputs = with pkgs; [ cmake pkg-config ];
+          buildInputs = with pkgs; [ fcitx5 ];
+
+          cmakeFlags = [
+            "-DCMAKE_BUILD_TYPE=Release"
+          ];
+        })
+      ];
+
+      fcitx5.waylandFrontend = true;
+    };
+
     # TODO: Maker german default, for learning
     defaultLocale = lib.mkDefault "en_AU.UTF-8";
 
